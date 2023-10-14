75c61667d39ac92eb55b26e536471a0c7a0111a5
AbelPalominoRojas <abel.palominorojas@gmail.com>
2023-10-14 12:19:28 -0500

    Add react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,120 +1,46 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 const App = (): JSX.Element => {
 	return (
 		<>
-			<nav className="navbar navbar-expand-lg bg-body-tertiary">
-				<div className="container-fluid">
-					<a className="navbar-brand" href="#">
-						Navbar
-					</a>
-					<button
-						className="navbar-toggler"
-						type="button"
-						data-bs-toggle="collapse"
-						data-bs-target="#navbarSupportedContent"
-						aria-controls="navbarSupportedContent"
-						aria-expanded="false"
-						aria-label="Toggle navigation"
-					>
-						<span className="navbar-toggler-icon"></span>
-					</button>
-					<div className="collapse navbar-collapse" id="navbarSupportedContent">
-						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-							<li className="nav-item">
-								<a className="nav-link active" aria-current="page" href="#">
-									Home
-								</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#">
-									Link
-								</a>
-							</li>
-							<li className="nav-item dropdown">
-								<a
-									className="nav-link dropdown-toggle"
-									href="#"
-									role="button"
-									data-bs-toggle="dropdown"
-									aria-expanded="false"
-								>
-									Dropdown
-								</a>
-								<ul className="dropdown-menu">
-									<li>
-										<a className="dropdown-item" href="#">
-											Action
-										</a>
-									</li>
-									<li>
-										<a className="dropdown-item" href="#">
-											Another action
-										</a>
-									</li>
-									<li>
-										<hr className="dropdown-divider" />
-									</li>
-									<li>
-										<a className="dropdown-item" href="#">
-											Something else here
-										</a>
-									</li>
-								</ul>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link disabled" aria-disabled="true">
-									Disabled
-								</a>
-							</li>
-						</ul>
-						<form className="d-flex" role="search">
-							<input
-								className="form-control me-2"
-								type="search"
-								placeholder="Search"
-								aria-label="Search"
-							/>
-							<button className="btn btn-outline-success" type="submit">
-								Search
-							</button>
-						</form>
-					</div>
-				</div>
-			</nav>
+			<Navbar expand="lg" className="bg-body-tertiary">
+				<Container>
+					<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="me-auto">
+							<Nav.Link href="#home">Home</Nav.Link>
+							<Nav.Link href="#link">Link</Nav.Link>
+							<NavDropdown title="Dropdown" id="basic-nav-dropdown">
+								<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+								<NavDropdown.Divider />
+								<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+							</NavDropdown>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
 
-			<div className="container-fluid">
-				<div className="row">
-					<div className="col-12">
-						<button type="button" className="btn btn-primary">
-							Primary
-						</button>
-						<button type="button" className="btn btn-secondary">
-							Secondary
-						</button>
-						<button type="button" className="btn btn-success">
-							Success
-						</button>
-						<button type="button" className="btn btn-danger">
-							Danger
-						</button>
-						<button type="button" className="btn btn-warning">
-							Warning
-						</button>
-						<button type="button" className="btn btn-info">
-							Info
-						</button>
-						<button type="button" className="btn btn-light">
-							Light
-						</button>
-						<button type="button" className="btn btn-dark">
-							Dark
-						</button>
-
-						<button type="button" className="btn btn-link">
-							Link
-						</button>
-					</div>
-				</div>
-			</div>
+			<Container>
+				<Row>
+					<Col xs={12}>
+						<Button variant="primary">Primary</Button>{' '}
+						<Button variant="secondary">Secondary</Button>{' '}
+						<Button variant="success">Success</Button> <Button variant="warning">Warning</Button>{' '}
+						<Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
+						<Button variant="light">Light</Button> <Button variant="dark">Dark</Button>
+						<Button variant="link">Link</Button>
+					</Col>
+				</Row>
+			</Container>
 		</>
 	);
 };
