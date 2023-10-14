ad27945551cb54011c5c450ba508ac6e0c71b909
AbelPalominoRojas <abel.palominorojas@gmail.com>
2023-10-14 13:57:29 -0500

    Mineral Type: List

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import '@popperjs/core';
@@ -10,7 +10,7 @@ import router from './core/router';
 import { RouterProvider } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>,
+	// <React.StrictMode>
+	<RouterProvider router={router} />,
+	// </React.StrictMode>,
 );
